3834e68025e8650ac8f0dde99aa97887b30f854d
김성득 <sdkim@ibleaders.co.kr>
2025-12-03 15:39:44 +0900

    Highcharts를 전역 객체에 설정 수정

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -3,8 +3,8 @@ import Highcharts from 'highcharts/highstock';
 import 'highcharts/highcharts-more';
 import 'highcharts/modules/series-label';
 
-// IBChart에서 전역 Highcharts 참조를 위해 window에 등록
-window.Highcharts = Highcharts;
+// Highcharts를 전역 객체에 설정
+if (typeof window !== 'undefined') window.Highcharts = Highcharts;
 
 const ibchartLib = {
   name: 'ibchart',
